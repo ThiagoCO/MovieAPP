5e7971b5fcb5f95d163ef2503d5d9ac4e589a57f
Thiago Cavalcante <thiago.cavalcante15@hotmail.com>
2019-01-12 14:46:52 -0200

    instalando o Cocoapods no projeto

---
+++ Movies/Services/APIManager.swift
@@ -0,0 +1,26 @@
+//
+//  APIManager.swift
+//  Movies
+//
+//  Created by Thiago Cavalcante de Oliveira on 12/01/19.
+//  Copyright © 2019 Thiago Cavalcante de Oliveira. All rights reserved.
+//
+
+import Foundation
+//
+//  APIManager.swift
+//  ChuckNorris
+//
+//  Created by Thiago Cavalcante de Oliveira on 27/11/2018.
+//  Copyright © 2018 Thiago Cavalcante de Oliveira. All rights reserved.
+//
+
+import Foundation
+
+
+class APIManager {
+    
+    static let shared = APIManager()
+    
+    
+}
